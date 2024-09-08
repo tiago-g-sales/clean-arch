112fa9ea313e3a3397494379ab00baa04a7f475c
Tiago Sales <tiagosales@outlook.com>
2024-09-08 15:46:23 -0300

    update files

---
+++ cmd/ordersystem/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/streadway/amqp"
+	"github.com/tiago-g-sales/clean-arch/configs"
+	"github.com/tiago-g-sales/clean-arch/internal/events"
+)
+
+func main() {
+
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := sql.Open(configs.DBDriver , fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", configs.DBUser, configs.DBPassword, configs.DBHost, configs.DBPort, configs.DBName))
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	rabbitMQChannel := getRabbitMQChannel()
+
+	eventDispatcher := events.N
+
+}
+
+
+func getRabbitMQChannel() *amqp.Channel{
+
+	configs, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+	conn, err := amqp.Dial(fmt.Sprintf("%s://%s:%s@%s:%s/", configs.MQDriver, configs.MQUser, configs.MQHost, configs.MQPort))
+	if err != nil {
+		panic(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	return ch
+
+}
+
